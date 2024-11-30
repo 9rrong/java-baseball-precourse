5881a580f46ae4e80bb56615eb028daebe8fb1f6
Goohong Chung <9rrong@uoslife.team>
2024-11-30 20:12:35 +0900

    fix(RandomNumbersGenerator): 서로 다른 3개의 수를 반환하도록 수정

--- src/main/java/baseball/model/RandomNumbersGenerator.java
+++ src/main/java/baseball/model/RandomNumbersGenerator.java
@@ -6,15 +6,10 @@ import static baseball.model.Numbers.NUMBER_MIN_VALUE;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class RandomNumbersGenerator {
 
-
     public List<Integer> getRandomNumbers() {
-        return IntStream.range(0, NUMBERS_LENGTH)
-                .mapToObj(i -> Randoms.pickNumberInRange(NUMBER_MIN_VALUE, NUMBER_MAX_VALUE))
-                .collect(Collectors.toList());
+        return Randoms.pickUniqueNumbersInRange(NUMBER_MIN_VALUE, NUMBER_MAX_VALUE, NUMBERS_LENGTH);
     }
 }
